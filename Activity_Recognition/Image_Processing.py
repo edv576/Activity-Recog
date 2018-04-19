e4b386417922761b5615c431e59306a31a680019
edv576 <31450210+edv576@users.noreply.github.com>
2018-04-19 13:31:36 +0200

    From gray to RGB

--- Activity_Recognition/Image_Processing.py
+++ Activity_Recognition/Image_Processing.py
@@ -41,7 +41,7 @@ def create_train_data():
     for img in tqdm(os.listdir(TRAIN_DIR)):
         label = label_img(img)
         path = os.path.join(TRAIN_DIR, img)
-        img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (IMAGE_SIZE, IMAGE_SIZE))
+        img = cv2.resize(cv2.imread(path, cv2.IMREAD_COLOR), (IMAGE_SIZE, IMAGE_SIZE))
         training_data.append([np.array(img), np.array(label)])
     shuffle(training_data)
     np.save('train_data.npy', training_data)
@@ -53,7 +53,7 @@ def process_test_data():
     for img in tqdm(os.listdir(TEST_DIR)):
         path = os.path.join(TEST_DIR, img)
         img_num = img.split('.')[0]
-        img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (IMAGE_SIZE, IMAGE_SIZE))
+        img = cv2.resize(cv2.imread(path, cv2.IMREAD_COLOR), (IMAGE_SIZE, IMAGE_SIZE))
         testing_data.append([np.array(img), img_num])
 
     np.save('test_data.npy', testing_data)
